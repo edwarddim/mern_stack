365905f35edc652569df81886c3a970dc8cd374b
Edward <eddieim@gmail.com>
2021-07-09 15:57:58 -0700

    W1D5 afternoon

--- form_demo/src/components/Form.jsx
+++ form_demo/src/components/Form.jsx
@@ -5,20 +5,21 @@ const Form = props => {
     // const [email, setEmail] = useState("INITIAL EMAIL STATE")
     // const [password, setPassword] = useState("INITIAL PW STATE")
     const [formState, setFormState] = useState({
-        email : "INTITIAL EMAIL STATE",
-        password : "INTITIAL PW STATE"
+        email : "",
+        password : "",
+        confirmPassword : ""
     })
 
     const [validState, setValidState] = useState({
         email : false,
-        password : false
+        password : false,
+        confirmPassword : false
     })
 
     const handleChange = event => {
         const {name, value} = event.target
-
         setFormState({
-            ...inputState,
+            ...formState,
             [name] : value
         })
         // setEmail(event.target.value)
@@ -27,8 +28,23 @@ const Form = props => {
     const handleSubmit = event => {
         event.preventDefault()
         // check for email validations
+        if(! (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).test(formState.email)){
+            var email = true
+        }
         // check for password validations
+        if(formState.password.length < 8){
+            var password = true
+        }
+        if(formState.password !== formState.confirmPassword){
+            var confirmPassword = true
+        }
         
+        setValidState({
+            ...validState,
+            email,
+            password,
+            confirmPassword
+        })
     }
 
     // const passwordChange = event => {
@@ -42,18 +58,25 @@ const Form = props => {
                 <p>
                     Email:
                     <input type="text" name="email" onChange={handleChange} id="" />
+                    { (validState.email) ? <p>Your email must be valid pattern</p> : null }
                 </p>
                 <p>
                     Password:
                     <input type="text" name="password" id="" onChange={handleChange} />
+                    { (validState.password) ? <p>Your password must be longer than 8 chars</p> : null }
+                </p>
+                <p>
+                    Confirm Password:
+                    <input type="text" name="confirmPassword" id="" onChange={handleChange} />
+                    { (validState.confirmPassword) ? <p>Your password and cofirm password must match</p> : null }
                 </p>
                 <button type="submit">Login</button>
             </form>
             <div>
                 {/* <p>{email}</p>
                 <p>{password}</p> */}
-                <p>{inputState.email}</p>
-                <p>{inputState.password}</p>
+                <p>{formState.email}</p>
+                <p>{formState.password}</p>
             </div>
         </div>
     )
