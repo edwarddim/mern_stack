5cbeddbc6ec669282132b77da3e916a299614dbe
Edward <eddieim@gmail.com>
2021-07-15 11:34:09 -0700

    W2D4 morning

---
+++ server_demo/server.js
@@ -0,0 +1,31 @@
+// 1. IMPORT EXPRESS AND CREATE AN EXPRESS SERVER
+const express = require("express") // import express from "express"
+const app = express() // INSTANTIATING AN EXPRESS SERVER
+const port = 8000
+
+// 2. CONFIGURE YOUR EXPRESS SERVER
+app.use( express.json() ); // ALLOWS JSON USAGE
+app.use( express.urlencoded({ extended: true }) ); // ACCEPT POST REQUESTS
+const users = [
+    { firstName: "Reimu",  lastName: "Hakurei"    },
+    { firstName: "Marisa", lastName: "Kirisame"   },
+    { firstName: "Sanae",  lastName: "Kochiya"    },
+    { firstName: "Sakuya", lastName: "Izayoi"     },
+    { firstName: "Momiji", lastName: "Inubashiri" }
+];
+// 3. DEFINE ALL THE URLS FOR YOUR EXPRESS SERVER
+app.get("/api/users", (request, respsonse) => {
+    var dbInfo = users
+    respsonse.json(dbInfo)
+})
+
+app.post("/api/users", (req, res) => {
+    console.log(req.body)
+    res.json({
+        "message" : "This is the POST /api/users route"
+    })
+})
+ 
+
+// 4. RUN YOUR EXPRESS SERVER
+app.listen(port, () => console.log(`SERVER IS RUNNIG ON PORT ${port}`) )
