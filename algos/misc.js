bde783d1b6e7946d188b66ff5f25c604690870f3
Edward <eddieim@gmail.com>
2021-08-26 10:43:08 -0700

    final July commit

--- algos/misc.js
+++ algos/misc.js
@@ -242,3 +242,161 @@ const expected4 = 4;
  * - Space: O(?).
  */
 function lengthOfLongestSubString(str) {}
+
+
+/* 
+  https://leetcode.com/problems/compare-version-numbers/
+  Given two strings, version1, and version2, both representing version numbers
+  If version1 > version2 return 1; if version1 < version2 return -1; otherwise return 0.
+  Helpful methods:
+    - .split(characterToSplitOn)
+      - returns an array of items: "a-b-c".split("-") returns ["a", "b", "c"]
+    - .parseInt
+      - given a string, converts it to and returns int or NaN (Not a Number) if conversion fails
+*/
+
+const test1V1 = "0.1";
+const test1V2 = "1.1";
+const expected1 = -1;
+
+const test2V1 = "1.0.1";
+const test2V2 = "1";
+const expected2 = 1;
+
+const test3V1 = "7.5.2.4";
+const test3V2 = "7.5.3";
+const expected3 = -1;
+
+const test4V1 = "7.5.2.4";
+const test4V2 = "7.5.2";
+const expected4 = 1;
+
+const test5V1 = "1.01";
+const test5V2 = "1.001";
+const expected5 = 0;
+// Explanation: Ignoring leading zeroes, both “01” and “001" represent the same number “1”
+
+const test6V1 = "1.0.1";
+const test6V2 = "1";
+const expected6 = 1;
+
+/**
+ * Determines which version number is greater or if they are equal.
+ * @param {string} v1
+ * @param {string} v2
+ * @returns {number} 1 if v1 greater, -1 if v1 smaller, 0 if equal.
+ * - Time: O(?).
+ * - Space: O(?).
+ */
+function compareVersionNumbers(v1, v2) {}
+
+
+/* 
+https://leetcode.com/problems/container-with-most-water/
+*/
+
+const heights1 = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+const expected1 = 49;
+
+const heights2 = [1, 1];
+const expected1 = 1;
+
+const heights3 = [4, 3, 2, 1, 4];
+const expected3 = 16;
+
+const heights4 = [1, 2, 1];
+const expected4 = 2;
+
+/**
+ * Finds the max area of a container from the given heights where the length
+ * is the distance between indexes.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {number[]} heights
+ * @returns {number} Representing the max area of a container.
+ */
+function containerWithMostWater(heights) {}
+
+
+/* 
+Amazon is developing a new song selection algorithm that will sync with the
+duration of your bus ride.
+Given a positive integer representing a duration of a bus ride and
+an array of positive integers representing song times, find a pair of two songs
+where the song pair ends 30 seconds before the bus ride ends.
+return an array of the song indexes or [-1, -1] if no pair is found.
+If there are multiple song pairs that match, return the pair that contains the
+longest song. The order of the returned indexes doesn't matter.
+*/
+
+const busDuration1 = 300;
+const songDurations1 = [70, 120, 200, 45, 210, 40, 60, 50];
+const expected1 = [4, 6]; // 210, 60
+/* Explanation:
+There are multiple pairs that add up to 30 seconds before the bus duration.
+The pair at indexes 4 and 6 is the pair with the longest song that is chosen.
+*/
+
+const busDuration2 = 300;
+const songDurations2 = [70, 120, 200, 230, 45, 210, 40, 60, 50];
+const expected2 = [3, 6]; // 230, 40
+/* Explanation:
+This is the pair with the longest song.
+*/
+
+const busDuration3 = 300;
+const songDurations3 = [70, 120, 20, 23, 45, 21, 40, 60, 50];
+const expected3 = [-1, -1]; // not found.
+
+/**
+ * Finds the pair of song durations that adds up to 30 seconds before the bus
+ * ride ends.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {number} busDuration Seconds.
+ * @param {number} songDurations Seconds.
+ * @returns {Array<number, number>} The song pair indexes, or [-1, -1] if no
+ *    pair is found.
+ */
+function amazonMusicRuntime(busDuration, songDurations) {}
+
+
+/* 
+  Given two strings,
+  return true if the first string can be built from the letters in the 2nd string
+  Ignore case
+  .indexOf will only tell you if the letter is found one time
+*/
+
+const strA1 = "Hello World";
+const strB1 = "lloeh wordl";
+const expected1 = true;
+
+const strA2 = "Hey";
+const strB2 = "hello";
+const expected2 = false;
+// Explanation: second string is missing a "y"
+
+const strA3 = "hello";
+const strB3 = "helo";
+const expected3 = false;
+// Explanation: second string doesn't have enough "l" letters
+
+const strA4 = "hello";
+const strB4 = "lllheo";
+const expected4 = true;
+
+const strA5 = "hello";
+const strB5 = "heloxyz";
+const expected5 = false;
+// Explanation: not strB5 does not have enough "l" chars.
+
+/**
+ * Determines whether s1 can be built using the chars of s2.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} s1
+ * @param {string} s2
+ * @returns {boolean}
+ */
+function canBuildS1FromS2(s1, s2) {}
---
+++ project_presentations.md
@@ -0,0 +1,16 @@
+## INTRODUCE YOURSELEF(VES) AND YOUR PROJECT
+
+## WHAT STACK IS THE PROJECT IN?
+
+## WHY DID YOU DECIDE ON THAT STACK?
+ - What is the reasoning behind your choice?
+ - Talk about advantages AND disadvantages
+
+## PRESENTATION VS CODE REVIEW
+ - PRESENT your project, DO NOT SHOW CODE
+
+## TALK ABOUT YOUR STRUGGLES DURING DEVELOPMENT
+ - What kind of issues did you run into? How did you get around said issues
+
+## Practice
+ - Just like you would practice for an interview, practice for your presentation
