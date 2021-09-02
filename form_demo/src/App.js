7456c2b8d14e94932a33bd44c2f1d1c27328c3cf
Edward <eddieim@gmail.com>
2021-09-02 15:55:14 -0700

    W1D4 afternoon

--- form_demo/src/App.js
+++ form_demo/src/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 import Counter from './components/Counter';
 import Form from './components/Form';
 import CombineForm from './components/CombineForm';
+import Registration from './components/Registration';
 
 function App() {
   return (
       <fieldset>
         <legend>App.js</legend>
-        <Counter />
+        {/* <Counter /> */}
         {/* <Form /> */}
-        <CombineForm />
+        {/* <CombineForm /> */}
+        <Registration />
       </fieldset>
   );
 }
