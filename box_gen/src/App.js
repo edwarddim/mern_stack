e643785c06f50083f233ebf87ce07ace54a41b32
Edward <eddieim@gmail.com>
2021-09-03 11:10:34 -0700

    W1D5 morning

---
+++ box_gen/src/App.js
@@ -0,0 +1,23 @@
+import BoxDisplay from "./components/BoxDisplay";
+import Form from "./components/Form";
+import {useState} from 'react';
+
+function App() {
+
+  const [arrState, setArrState] = useState([]);
+
+  const addToArray = (color) => {
+    setArrState([...arrState, color])
+  }
+
+
+  return (
+    <fieldset>
+      <legend>App.js</legend>
+      <Form addToArray={addToArray} />
+      <BoxDisplay colors={arrState} />
+    </fieldset>
+  );
+}
+
+export default App;
