908a7c1fc6e27e3b652024e69512de7a9701d7eb
Edward <eddieim@gmail.com>
2021-11-01 10:57:16 -0700

    W2D1 morning

---
+++ messageboard_demo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MessageBoard from './components/MessageBoard';
+
+function App() {
+  return (
+    <div className="App">
+      <MessageBoard />
+    </div>
+  );
+}
+
+export default App;
