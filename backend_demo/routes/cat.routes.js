17be42665910043be29c3d50aea128f99e68d897
Edward Im <eddieim@gmail.com>
2022-04-29 16:44:00 -0700

    start of May 2022

--- backend_demo/routes/cat.routes.js
+++
@@ -1,6 +0,0 @@
-const CatController = require("../controllers/cat.controller")
-
-module.exports = app => {
-    app.get("/api/cats", CatController.findAllCats)
-    app.post("/api/cats/new", CatController.createCat)
-}
