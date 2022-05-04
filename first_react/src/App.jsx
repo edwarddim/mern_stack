c6e85ce13062f36055cd6b98029b07971b7dd2f5
Edward Im <eddieim@gmail.com>
2022-05-04 16:07:05 -0700

    W1D3 afternoon

--- first_react/src/App.jsx
+++ first_react/src/App.jsx
@@ -3,15 +3,37 @@ import './App.css';
 // IMPORTING ALL THE COMPNONENTS
 import Display from './components/Display';
 import Form from './components/Form';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <fieldset>
-      <legend>App.js</legend>
-      <Display />
-      <Form />
-    </fieldset>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      clicked : 0,
+      name : "Edward Im"
+    }
+  }
+
+  clickHandler = () => {
+    // console.log("I have been clicked")
+    // console.log(this.state.clicked)
+    let {clicked} = this.state
+    this.setState({
+      'clicked' : clicked+1,
+    })
+    // console.log(this.state)
+  } 
+
+  render(){
+      return (
+        <fieldset>
+        <legend>App.js</legend>
+        <button onClick={this.clickHandler}>Click Me!</button>
+        <Display clicked={this.state.clicked} firstName="Edward" lastName="Im" age={32} hairColor="Black" />
+        <Form />
+      </fieldset>
+    );
+  }
 }
 
 export default App;
--- first_react/src/components/Display.jsx
+++ first_react/src/components/Display.jsx
@@ -5,10 +5,15 @@ import React, {Component} from "react"
 // CREATE THE COMPONENT
 class Display extends Component{
     render(){
+        const {firstName, lastName, age, hairColor, clicked} = this.props
         return (
             <fieldset>
                 <legend>Display.jsx</legend>
-                <h1>Hello!</h1>
+                <h1>First Name: {firstName}</h1>
+                <h1>Last Name: {lastName}</h1>
+                <h1>Age: {age}</h1>
+                <h1>Hair Color: {hairColor}</h1>
+                <h1>Number clicked: {clicked}</h1>
             </fieldset>
         )
     }
