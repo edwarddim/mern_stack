393a93885bd641b3815844dbfc2075d9f526a611
Edward Im <eddieim@gmail.com>
2022-05-11 16:06:11 -0700

    W2D3 afternoon

--- router_demo/src/App.js
+++ router_demo/src/App.js
@@ -1,24 +1,33 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Navbar from './components/Navbar';
-import Auth from './views/Auth';
-import Create from './views/Create';
-import Dashboard from './views/Dashboard';
-import Detail from './views/Detail';
-import Edit from './views/Edit';
+// import Navbar from './components/Navbar';
+import PokeForm from './components/PokeForm';
+import Pokemon from './views/Pokemon';
+// import Auth from './views/Auth';
+// import Create from './views/Create';
+// import Dashboard from './views/Dashboard';
+// import Detail from './views/Detail';
+// import Edit from './views/Edit';
 
 function App() {
   return (
     <BrowserRouter>
       <fieldset>
         <legend>App.js</legend>
-        <Navbar />
+        <Routes>
+          <Route path="/" element={<PokeForm />}>
+            <Route path="pokemon/:pokemonName" element={<Pokemon />} />
+          </Route>
+        </Routes>
+
+
+        {/* <Navbar />
         <Routes>
           <Route path="/" element={<Auth />} />
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="recipes/new" element={<Create />} />
           <Route path="recipes/:recipe_id" element={<Detail />} />
           <Route path="recipes/:recipe_id/edit" element={<Edit />} />
-        </Routes>
+        </Routes> */}
       </fieldset>
     </BrowserRouter>
   );
