5ce1fa8b8dd0e7d985a9799dc2cafd58807a0f9e
Edward Im <eddieim@gmail.com>
2022-05-14 10:38:03 -0700

    end of W2

---
+++ mongoDb/HelloMongoose/server/models/HeroModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const HeroSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, 'Please enter name.']
+	},
+	alias: {
+		type: String,
+		required: [true, 'Please enter alias.']
+	}
+}, { timestamps: true });
+
+const Hero = mongoose.model("Hero", HeroSchema);
+
+module.exports = Hero;
