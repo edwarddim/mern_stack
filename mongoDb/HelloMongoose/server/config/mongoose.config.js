2c5fa24b0f38c140d631ac49f25a09af67256dd6
Edward Im <eddieim@gmail.com>
2022-06-05 12:53:52 -0700

    delete

--- mongoDb/HelloMongoose/server/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost/heroDb", {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-})
-	.then(() => console.log(`Established connection to ${mongoose.connection.name}`))
-	.catch(err => console.log(`Something went wrong: ${err}`));
