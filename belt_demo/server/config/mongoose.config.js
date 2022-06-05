c2105daf2c6255c31af6b3ef83e3c4bf073cb1c9
Edward Im <eddieim@gmail.com>
2022-06-05 12:58:38 -0700

    delete

--- belt_demo/server/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-mongoose.connect("mongodb://localhost/belt_exam_1",{
-    useNewUrlParser: true,
-	useUnifiedTopology: true,
-})
-.then(()=> console.log("CONNECTED TO BELT DEMO DB 1"))
-.catch(err => console.log("ERROR: ", err))
