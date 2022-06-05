5d996e13843d0d08a5a5095e79682ad9041c082a
Edward Im <eddieim@gmail.com>
2022-06-05 12:57:15 -0700

    delete

--- express_demo/routes/user.routes.js
+++
@@ -1,6 +0,0 @@
-const UserController = require("../controllers/user.controllers")
-
-module.exports = app => {
-    app.get("/api/users", UserController.allUsers)
-    app.post("/api/users", UserController.createUser)
-}
