8a50966a2532a16aa9b27a445e72c764f04051bb
Edward Im <eddieim@gmail.com>
2022-07-12 10:53:55 -0700

    W2D1

---
+++ RESTful_api.md
@@ -0,0 +1,9 @@
+# What is an API?
+ - Application Programming Interface
+
+
+# STATUS CODE
+ - 200 OK
+ - 300 
+ - 400 CLIENT ERROR (404)
+ - 500 SERVER ERROR 
---
+++ api_demo/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <fieldset>
+      <legend>App.js</legend>
+      <Form />
+    </fieldset>
+  );
+}
+
+export default App;
