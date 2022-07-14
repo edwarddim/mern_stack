823b8abea447be8c98e37266274d33b6a81c0ebc
Edward Im <eddieim@gmail.com>
2022-07-14 15:48:55 -0700

    W2D4 afternoon

---
+++ express_demo/Superjeroes.js
@@ -0,0 +1,54 @@
+const superheroes = [
+    {
+        id: 1,
+        name: 'Spider-Man',
+        alias: 'Peter Parker'
+    },
+    {
+        id: 2,
+        name: 'Squirrel Girl',
+        alias: 'Doreen Green'
+    },
+    {
+        id: 3,
+        name: 'Wolverine',
+        alias: 'James Howlett'
+    },
+    {
+        id: 4,
+        name: 'Black Widow',
+        alias: 'Natasha Romanov'
+    },
+    {
+        id: 5,
+        name: 'Captain America',
+        alias: 'Steve Rogers'
+    },
+    {
+        id: 6,
+        name: 'Scarlet Witch',
+        alias: 'Wanda Maximoff'
+    },
+    {
+        id: 7,
+        name: 'Daredevil',
+        alias: 'Matt Murdock'
+    },
+    {
+        id: 8,
+        name: 'Storm',
+        alias: 'Ororo Munroe'
+    },
+    {
+        id: 9,
+        name: 'Master of Kung Fu',
+        alias: 'Shang-Chi'
+    },
+    {
+        id: 10,
+        name: 'Echo',
+        alias: 'Maya Lopez'
+    },
+];
+
+module.exports = superheroes;
--- express_demo/server.js
+++ express_demo/server.js
@@ -1,7 +1,6 @@
 // 1. IMPORT YOUR DEPENDENCIES
 const express = require("express");
 const { faker } = require("@faker-js/faker")
-// import { faker } from '@faker-js/faker';
 
 // 2. INSTANTIATE AN EXPRESS SERVER
 const app = express();
@@ -13,26 +12,8 @@ app.use(express.urlencoded({ extended: true }))
 
 
 // 3. DEFINE YOUR API ENDPOINTS
-app.get("/api/hello", (request, response) => { // GET localhost:8000/api/hello
-    response.json({ message: "Hello" })
-})
-
-
-app.get("/api/users", (request, respsone) => {
-    respsone.json([1, 2, 3, 4, 5, 6])
-})
-
-app.post("/api/users", (req, res) => {
-    res.json({
-        userId: faker.datatype.uuid(),
-        username: faker.internet.userName(),
-        email: faker.internet.email(),
-        avatar: faker.image.avatar(),
-        password: faker.internet.password(),
-        birthdate: faker.date.birthdate(),
-        registeredAt: faker.date.past(),
-    });
-})
+const superheroRoutes = require("./routes/superhero.routes")
+superheroRoutes(app)
 
 // 4. RUN YOUR EXPRESS SERVER
 app.listen(port, () => console.log('Listening on port 8000'));
