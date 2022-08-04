608b8318aed41ed9afe5ac55fbbe302c564154c2
Edward Im <eddieim@gmail.com>
2022-08-04 11:19:11 -0700

    reset master

--- boilerplate_demo/server/config/mongoose.config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-
-mongoose.connect("mongodb://localhost/boilerplate_demo",{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
-.then(()=> console.log("CONNECTED TO BOILERPLATE DEMO"))
-.catch(err => console.log("ERROR: ", err))
--- mongoDb/HelloMongoose/server/routes/heroRoutes.js
+++
@@ -1,8 +0,0 @@
-const HeroController = require('../controllers/heroController');
-
-module.exports = (app) => {
-  app.get("/api/heroes", HeroController.findAll);
-  app.post("/api/heroes", HeroController.create);
-  app.get("/api/heroes/:id", HeroController.findOne);
-  app.put("/api/heroes/:id", HeroController.update);
-  app.delete("/api/heroes/:id", HeroController.delete);
